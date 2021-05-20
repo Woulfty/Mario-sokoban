a87f1fa95ea9e78b0380e0864722d2e890db4c49
leabernard <70693222+leabernard@users.noreply.github.com>
2021-05-20 18:56:07 +0200

    modif

--- jeu.h
+++ jeu.h
@@ -2,6 +2,6 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
-void jouer(sf::RenderWindow* Window);
+int jouer(sf::RenderWindow* Window);
 void deplacerJoueur(int carte[][NB_BLOCS_HAUTEUR], sf::Vector2i * pos, int direction);
 void deplacerCaisse(int *premiereCase, int *secondeCase);
